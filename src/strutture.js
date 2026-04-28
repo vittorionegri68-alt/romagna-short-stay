165ac531ee0abd281e6f30e6da15e1f4214ee69e
vittorionegri68-alt <vittorionegri68@gmail.com>
2026-04-28 14:55:49 +0200

    Set prezzo_da to 145 in strutture.js

--- src/strutture.js
+++ src/strutture.js
@@ -173,7 +173,7 @@ export const strutture = [
     ],
     rating: 4.88,
     recensioni: 0,
-    prezzo_da: null,
+    prezzo_da: 145,
     url: 'https://samilla.romagna-short-stay.com/',
     airbnb: 'https://www.airbnb.it/rooms/1618206280550914997',
     attivo: true,
